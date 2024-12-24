f7fa1df6098bd4684c8398ccd8df6e73ae4a6b75
AdieDev0 <devutella@gmail.com>
2024-12-24 15:00:28 +0800

    Update

--- liberty/src/Components/Hero.jsx
+++ liberty/src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Marquee from "react-fast-marquee";
-import { motion } from "framer-motion"; // Import motion from framer-motion
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const slides = [
